324124af9e8cf1dc2bc7742ffd9aaaacfec6bb3e
Eleazar <eleazar.cortezquirino@utxicotepec.edu.mx>
2022-07-27 20:36:14 -0500

    Manejo de lista de U. via Sockets y S. REST

--- src/app/components/list-usuarios/list-usuarios.component.ts
+++ src/app/components/list-usuarios/list-usuarios.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChatService } from '../../services/chat.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-list-usuarios',
   templateUrl: './list-usuarios.component.html',
   styleUrls: ['./list-usuarios.component.css']
 })
 export class ListUsuariosComponent implements OnInit {
+  usuariosActivosObs!: Observable<any>;
+  constructor(
+    public ChatService: ChatService
+  ) { }
 
-  constructor() { }
+  ngOnInit(){
+    this.usuariosActivosObs = this.ChatService.getUsuariosActivos();
 
-  ngOnInit(): void {
+    this.ChatService.emitirUsuariosActivos();
+
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -7,24 +7,34 @@ import { WebsocketService } from './websocket.service';
 export class ChatService {
 
   constructor(
-    public wsService: WebsocketService) { }
+    public wsService: WebsocketService
+  ) { }
 
-  sendMessage(mensaje: string)
-  {
-    const payload = {
-      de: this.wsService.getUsuario().nombre,
-      cuerpo: mensaje,
-      color: 'aqua'
-    };
+    sendMessage( mensaje: string ) {
 
-    this.wsService.emit('mensaje', payload);
-  }
-  getMessages(){
-   return this.wsService.listen('mensaje-nuevo');
-  }
+      const payload = {
+        de: this.wsService.getUsuario().nombre,
+        cuerpo: mensaje
+      };
 
-  getMessagesPrivate(){
-    return this.wsService.listen('mensaje-privado')
-  }
+      this.wsService.emit( 'mensaje' , payload );
+
+    }
+
+    getMessages() {
+      return this.wsService.listen( 'mensaje-nuevo' );
+    }
+
+    getMessagesPrivate() {
+      return this.wsService.listen( 'mensaje-privado' );
+    }
+
+    getUsuariosActivos() {
+      return this.wsService.listen( 'usuarios-activos' );
+    }
+
+    emitirUsuariosActivos() {
+      this.wsService.emit('obtener-usuarios');
+    }
 
 }
